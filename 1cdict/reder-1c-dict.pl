916704eeda80bbabc594569deeb17df59a0417e0
FreeArcher <bespalov.alexey@yandex.ru>
2022-02-02 08:17:13 +0700

    Реализовал разбор словаря 1С

---
+++ 1cdict/reder-1c-dict.pl
@@ -0,0 +1,50 @@
+use strict;
+use utf8;
+
+use Data::Dumper::Simple;
+use Win32::Console;
+use Win32::Unicode::File;
+Win32::Console::OutputCP( 65001 );
+
+binmode(STDIN,':utf8');
+binmode(STDOUT,':utf8');
+
+
+my $filename = "1cdict.txt";
+open(my $fh, '<:encoding(UTF-8)', $filename)
+    or die "Could not open file '$filename' $!";
+
+my @lines;
+while (my $line = <$fh>) {
+    push(@lines, $line);
+}
+close($fh);
+
+my $newfilename = "dict.txt";
+open(my $fw, '>:encoding(UTF-8)', $newfilename) 
+    or die "Could not open file '$newfilename' $!";
+
+my $ru = "";
+my $en = "";
+foreach my $line (@lines) {
+    #{"ru","WSСсылки"},
+    #{"en","WSReferences"}
+    if ($line =~ /^{"ru","(\w+)"}/) {
+        $ru = $1; 
+        #print "$ru\n";
+
+    }
+    if ($line =~ /^{"en","(\w+)"}/) {
+        $en = $1; 
+        #print "$en\n";
+    }
+
+    if ($ru ne "" && $en ne "") {
+        print $fw "$ru - $en\n";
+        $ru = "";
+        $en = "";
+    }
+}
+print("Done \n")
+close($fw);
+
