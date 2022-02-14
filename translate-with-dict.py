1a3fabff24116e132485b85b29e938d8a38d9228
FreeArcher <bespalov.alexey@yandex.ru>
2022-02-14 22:17:43 +0700

    Реализовал перевод по словарю на phyton

--- translate-with-dict.py
+++ translate-with-dict.py
@@ -1,13 +1,14 @@
 import re
 import argparse
 
-
-filedict = 'dict-test.txt'
+filedict = 'dict.txt'
 dict_ru_en = list()
 
 with open(filedict, "r", encoding="utf-8") as f:
-    for str in f.readlines():
-        params = re.findall("^(\w+) - (\w+)$", str)
+    for line in f.readlines():
+        if (line.startswith("#") or line == ""):
+            continue
+        params = re.findall(r"^(\w+) - (\w+)$", line)
         dict_ru_en.append(params)
 
 parser = argparse.ArgumentParser()
@@ -31,19 +32,20 @@ lines = list()
 with open(filename, "r", encoding="utf-8") as f:
     lines = f.readlines()
 
+count = 0
 with open(filename_en, "w", encoding="utf-8") as f:
     for reg in dict_ru_en:
         for line in lines:
-        # old_data = old_data.replace(reg[0][0], reg[0][1])
-            pattern1 = '[\s(=]'+reg[0][0]+'[\s);,=]'
-            pattern2 = '^'+reg[0][0]+'[\s);,=]'
-            pattern3 = '[\s(=]'+reg[0][0]+'$'
-            repl = reg[0][1]
-            line = re.sub(pattern1, repl, line)
-            line = re.sub(pattern2, repl, line)
-            line = re.sub(pattern3, repl, line)
-            f.write(line)
-# with open(filename_en, "w", encoding="utf-8") as f:
-#         f.write(old_data)
+            ru = reg[0][0]
+            en = reg[0][1]
+            pattern = fr"\b{ru}\b"
+            newline = re.sub(pattern, en, line)
+            if newline != line:
+                count += 1
 
-print ("Done!")
+            lines[lines.index(line)] = newline
+
+    for line in lines:
+        f.write(line)
+
+print(f"Done! Repaleces {count} lines")
